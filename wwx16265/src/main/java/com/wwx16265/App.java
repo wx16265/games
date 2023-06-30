c3b90d9e0fbc2005ba896f31d4bd7b540b40a73a
wwx16265 <wwx16265@student.wsb.warszawa.pl>
2023-06-30 20:22:10 +0200

    added driver code

--- wwx16265/src/main/java/com/wwx16265/App.java
+++ wwx16265/src/main/java/com/wwx16265/App.java
@@ -2,6 +2,15 @@ package com.wwx16265;
 
 public class App {
     public static void main(String[] args) {
-    	
+        game_menu menu = new game_menu();
+        
+        try
+        {
+        	menu.run();
+        }
+        catch(Exception e)
+        {
+        	System.out.println("The game has encountered an unresolvable problem: " + e.getMessage());
+        }
     }   
 }
